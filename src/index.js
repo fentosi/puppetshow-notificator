a032fdef04bad1ccf29f782ce7eebc9929c5f327
fentosi <fentosi.adam@gmail.com>
2019-12-24 10:21:10 +0100

    Store shows in Firestore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const puppetShow = require('./puppetShow');
 const mailer = require('./mailer');
 const dotenv = require('dotenv');
+const { Firestore } = require('@google-cloud/firestore');
 
 dotenv.config();
 
 const dayGroups = process.env.DAY_GROUP.split(',').map((group) => group.trim());
 const ageGroups = process.env.AGE_GROUP.split(',').map((group) => group.trim());
+const keyFilename = 'gcp-credentials.json';
+const projectId = process.env.FIRESTORE_PROJECT_ID;
 
 const main = async () => {
+
+    const firestore = new Firestore({projectId, keyFilename});
+    let collectionRef = firestore.collection('shows');
+
     const shows = await puppetShow.getShows(ageGroups, dayGroups);
+    shows.forEach(async (show) => {
+        await collectionRef.add(show);
+    });
 
     if (shows.length > 0) {
         const emailContent = mailer.getEmailContent(shows);
