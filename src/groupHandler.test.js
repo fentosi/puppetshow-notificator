64e14e8aa3afde03983c86de4a398b8779b335ff
Adam Fentosi <fentosi.adam@gmail.com>
2026-01-11 14:48:51 +0100

    tests for group handler

---
+++ src/groupHandler.test.js
@@ -0,0 +1,29 @@
+const { isWithinAgeGroup, isWithinDayGroup } = require('./groupHandler');
+
+describe('groupHandler', () => {
+  describe('isWithinAgeGroup', () => {
+
+    [
+      { ageGroups: [], expectedValue: true },
+      { ageGroups: [2, 4, 6], expectedValue: true },
+      { ageGroups: [3,9], expectedValue: false }
+    ].forEach(({ ageGroups, expectedValue }) => {
+      it(`returns ${expectedValue} for ageGroups: ${JSON.stringify(ageGroups)}`, () => {
+        expect(isWithinAgeGroup(ageGroups, 4)).toBe(expectedValue);
+      });
+    });
+  });
+
+  describe('isWithinDayGroup', () => {
+    [
+      { dayGroups: [], expectedValue: true },
+      { dayGroups: ['Monday', 'Tuesday'], expectedValue: true },
+      { dayGroups: ['Wednesday', 'Friday'], expectedValue: false },
+    ].forEach(({ dayGroups, expectedValue }) => {
+      it(`returns ${expectedValue} for dayGroup: ${JSON.stringify(dayGroups)}`, () => {
+        expect(isWithinDayGroup(dayGroups, 'Tuesday')).toBe(expectedValue);
+      })
+    });
+  });
+});
+
