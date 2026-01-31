194e87c5ff54b96618d6abc37b12d3360eb87b45
Adam Fentosi <fentosi.adam@gmail.com>
2026-01-31 22:21:20 +0100

    script can be run in dry mode

--- scripts/sendEmailNotification.js
+++ scripts/sendEmailNotification.js
@@ -1,5 +1,8 @@
 const dotenv = require('dotenv');
 const { sendEmailNotificationAboutNewShows } = require("../src/getEmailNotificationAboutNewShows");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).parse()
 
 dotenv.config();
 
@@ -7,6 +10,6 @@ const dayGroups = process.env.DAY_GROUP.split(',').map((group) => group.trim());
 const ageGroups = process.env.AGE_GROUP.split(',').map((group) => group.trim());
 
 (async () => {
-  await sendEmailNotificationAboutNewShows(ageGroups, dayGroups);
+  await sendEmailNotificationAboutNewShows(ageGroups, dayGroups, !!argv.dryRun);
   process.exit(0);
 })();
