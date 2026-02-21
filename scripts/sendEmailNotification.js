6f77f7744ed171e15b3120d77d8542e8364d5199
Adam Fentosi <fentosi.adam@gmail.com>
2026-02-21 17:09:18 +0100

    connect to mongo when the script runs

--- scripts/sendEmailNotification.js
+++ scripts/sendEmailNotification.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const { sendEmailNotificationAboutNewShows } = require("../src/sendEmailNotificationAboutNewShows");
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
+const mongo = require("../helpers/mongo");
 const argv = yargs(hideBin(process.argv)).parse()
 
 dotenv.config();
@@ -10,6 +11,8 @@ const dayGroups = process.env.DAY_GROUP?.split(',').map((group) => group.trim())
 const ageGroups = process.env.AGE_GROUP?.split(',').map((group) => group.trim());
 
 (async () => {
+  await mongo.connect(process.env.MONGO_URI);
   await sendEmailNotificationAboutNewShows(ageGroups, dayGroups, !!argv.dryRun);
+  await mongo.disconnect()
   process.exit(0);
 })();
